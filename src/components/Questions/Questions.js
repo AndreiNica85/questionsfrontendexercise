66344b777fd7ac3c928c5945120a4fe712526262
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-11-07 14:21:05 +0100

    asd

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -2,6 +2,18 @@ import React from "react";
 
 function Questions(){
 
+
+// /questions?n=1
+
+// Fetching all questions.
+
+
+// When we answer a question (useState()) we save the answer and we go to the next one
+
+// return <Question id={} />
+
+
+
 }
 
 export default Questions;
