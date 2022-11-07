6a959ef5127bc78a1227623e82c7a7536b3afd1f
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-11-07 15:14:42 +0100

    fetch data

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,16 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Questions(){
 
 
 // /questions?n=1
-
+const [questions, setQuestions] = useState([]);
 // Fetching all questions.
+
+
+
+
+useEffect(() => {
+     let url = 'http://localhost:8080/questions'
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setQuestions(json)
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+}, []);
+
+
+console.log(questions);
+
 
 
 // When we answer a question (useState()) we save the answer and we go to the next one
 
-// return <Question id={} />
+// return <Question id={} /> /asdasdasdasd
+
+
 
 
 
